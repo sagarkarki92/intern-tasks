d6a6cee375a00c1871161ee33661455c5d98c14e
sagnar <sagarkarki184@gmail.com>
2019-05-26 15:45:37 +0545

    mario is moved. obstracles not created !!

--- js/mariogame.js
+++ js/mariogame.js
@@ -3,27 +3,27 @@
 //     this.ctx.fillRect(1000,420,100,100);
 //     this.ctx.fillRect(1000,400,100,25);
 
-var context, controller, rectangle, loop;
+var context, controller, rectangle, loop, obstracles;
 
 var canvas=document.getElementById("canvas");
 context=canvas.getContext('2d');
 
-rectangle={
-    height:42,
+rectangle = {
+    height:75,
     jumping:true,
-    width:75,
-    x:144,
-    x_velocity:0,
+    width:42,
+    x:10,
+    dx:0,
     y:0,
-    y_velocity:0,
+    dy:0,
 };
 
-controller={
+controller = {
 left:false,
 right:false,
 up:false,
-keyListener:function(event){
-    var key_state= (event.type=="keydown")?true:false;
+keyListener: function(event) {
+    var key_state= (event.type == "keydown")?true:false;
     switch(event.keycode)
     {
         case 37: //left arrow key
@@ -36,65 +36,75 @@ keyListener:function(event){
 
         case 39: //right arrow key
         this.right=key_state;
+        break;
     }
 }
 };
 
 loop=function(){
 
-    if(controller.up && rectangle.jumping == false)
+  if(controller.up && rectangle.jumping == false)
     {
-        rectangle.y_velocity-=20;
+        rectangle.dy-=20;
         rectangle.jumping=true;
     }
 
     if(controller.right){
-        rectangle.x_velocity+=0.5;
+        rectangle.dx += 0.5;
     }
 
     if(controller.left){
-        rectangle.x_velocity-=0.5;
+        rectangle.dx -=0.5;
     }
 
-    rectangle.y_velocity += 1.5;// gravity
-    rectangle.x += rectangle.x_velocity;
-    rectangle.y += rectangle.y_velocity;
-    rectangle.x_velocity *= 0.9;// friction
-    rectangle.y_velocity *= 0.9;// friction
+    rectangle.dy += 1.5;// gravity
+    rectangle.x += rectangle.dx;
+    rectangle.y += rectangle.dy;
+    rectangle.dx *= 0.9;// friction
+    rectangle.dy *= 0.9;// friction
 
-    // if rectangle is falling below floor line
-  if (rectangle.y > 180 - 16 - 32) {
+ 
+    if(rectangle.y > 520 - 75 - 16){
+      rectangle.jumping=false;
+      rectangle.y=520 -75 - 16;
+      rectangle.dy=0;
+    }
 
-    rectangle.jumping = false;
-    rectangle.y = 180 - 16 - 32;
-    rectangle.y_velocity = 0;
 
-  }
+    // if(rectangle.x > 1300){//if mario goes through right wall
+    //     rectangle.x= -42;
+    // }
 
-  // if rectangle is going off the left of the screen
-  if (rectangle.x < -32) {
+    // elseif(rectangle.x < -42) //if mario goes through left wall
+    // {
+    //     rectangle.x=1300;
+    // }
 
-    rectangle.x = 320;
 
-  } else if (rectangle.x > 320) {// if rectangle goes past right boundary
+    //background of canvas
+    context.fillStyle="black";
+    context.fillRect(0,0,1300,520);
+    context.beginPath();
+   
+    //mario
+    context.fillStyle="green";
+    context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+    context.fill();
+   context.fillStyle="red";
+    //obstracles and block
+    context.fillRect(375,275,400,50);
+    //top of tunnel i.e not being obstracles
+    context.fillRect(1000,405,100,100);
+    
+   context.fillRect(1000,385,100,25);
 
-    rectangle.x = -32;
-
-  }
-
-  context.fillStyle = "#202020";
-  context.fillRect(0, 0, 320, 180);// x, y, width, height
-  context.fillStyle = "#ff0000";// hex for red
-  context.beginPath();
-  context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-  context.fill();
-  context.strokeStyle = "#202830";
-  context.lineWidth = 4;
-  context.beginPath();
-  context.moveTo(0, 164);
-  context.lineTo(320, 164);
-  context.stroke();
-
+    //line buttom of the mario
+    context.strokeStyle='red';
+    context.lineWidth=4;
+    context.beginPath();
+    context.moveTo(0, 505);
+    context.lineTo(1300, 505);
+    context.stroke();
     
 
     window.requestAnimationFrame(loop);
